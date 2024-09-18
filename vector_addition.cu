a8731e62c2cf02fe5fdf987f5a811af4ec396c00
Andrea Mayer <andrea.mayer@uniroma2.it>
2024-09-18 20:54:38 +0000

    add NOPs and waste memory for stressing GPU
    to keep the GPU busy as much as possible, "NOP" instructions have been
    added, e.g. stressing for loop.
    
    On the memory side, the program allocates the GPU memory with a given
    expansion factor (default is 4) with respect to the host memory (CPU)
    containing useful data. This means that if the host useful data is 1MB,
    4*1MB of memory will be reserved on the GPU side. The computation is
    still performed on 1MB of GPU memory, resulting in 3MB of GPU memory
    being wasted.
    
    This is needed when we need to stress the GPU memory allocation, but we
    do not have enough host memory to launch the kernel on.
    
    Signed-off-by: Andrea Mayer <andrea.mayer@uniroma2.it>

--- vector_addition.cu
+++ vector_addition.cu
@@ -1,13 +1,37 @@
 #include <stdio.h>
+#include <cuda_runtime.h>
 
 // Size of array
-#define N 1048576
+#define N (128*1048576)
+#define THREADS_PER_BLOCK 1024
+#define SLOWDOWN_FACTOR 10000
+
+/* when a cudaMalloc(size) is called, the total amount of GPU memory is: size *
+ * CUDA_MALLOC_MEM_MUL.
+ */
+#define CUDA_MALLOC_MEM_MUL 4
+
+#define gpuErrorCheck(ans) { gpuAssert((ans), __FILE__, __LINE__); }
+void inline gpuAssert(cudaError_t code, const char *file, int line)
+{
+	if (code != cudaSuccess) {
+		fprintf(stderr, "GPUassert: %s (code=%d), file=%s, line=%d\n",
+			cudaGetErrorString(code), code, file, line);
+		exit(1);
+	}
+}
 
 // Kernel
 __global__ void add_vectors(double *a, double *b, double *c)
 {
+	int j;
 	int id = blockDim.x * blockIdx.x + threadIdx.x;
-	if(id < N) c[id] = a[id] + b[id];
+
+	if(id < N) {
+		// just for slowing down the whole operation
+		for(j=0; j < SLOWDOWN_FACTOR; ++j)
+			c[id] = a[id] + b[id];
+	}
 }
 
 // Main program
@@ -23,9 +47,9 @@ int main()
 
 	// Allocate memory for arrays d_A, d_B, and d_C on device
 	double *d_A, *d_B, *d_C;
-	cudaMalloc(&d_A, bytes);
-	cudaMalloc(&d_B, bytes);
-	cudaMalloc(&d_C, bytes);
+	gpuErrorCheck(cudaMalloc(&d_A, CUDA_MALLOC_MEM_MUL * bytes));
+	gpuErrorCheck(cudaMalloc(&d_B, CUDA_MALLOC_MEM_MUL * bytes));
+	gpuErrorCheck(cudaMalloc(&d_C, CUDA_MALLOC_MEM_MUL * bytes));
 
 	// Fill host arrays A and B
 	for(int i=0; i<N; i++)
@@ -35,28 +59,28 @@ int main()
 	}
 
 	// Copy data from host arrays A and B to device arrays d_A and d_B
-	cudaMemcpy(d_A, A, bytes, cudaMemcpyHostToDevice);
-	cudaMemcpy(d_B, B, bytes, cudaMemcpyHostToDevice);
+	gpuErrorCheck(cudaMemcpy(d_A, A, bytes, cudaMemcpyHostToDevice));
+	gpuErrorCheck(cudaMemcpy(d_B, B, bytes, cudaMemcpyHostToDevice));
 
 	// Set execution configuration parameters
 	//		thr_per_blk: number of CUDA threads per grid block
 	//		blk_in_grid: number of blocks in grid
-	int thr_per_blk = 256;
+	int thr_per_blk = THREADS_PER_BLOCK;
 	int blk_in_grid = ceil( float(N) / thr_per_blk );
 
 	// Launch kernel
 	add_vectors<<< blk_in_grid, thr_per_blk >>>(d_A, d_B, d_C);
 
 	// Copy data from device array d_C to host array C
-	cudaMemcpy(C, d_C, bytes, cudaMemcpyDeviceToHost);
+	gpuErrorCheck(cudaMemcpy(C, d_C, bytes, cudaMemcpyDeviceToHost));
 
 	// Verify results
-    double tolerance = 1.0e-14;
+	double tolerance = 1.0e-14;
 	for(int i=0; i<N; i++)
 	{
 		if( fabs(C[i] - 3.0) > tolerance)
 		{ 
-			printf("\nError: value of C[%d] = %d instead of 3.0\n\n", i, C[i]);
+			printf("\nError: value of C[%d] = %f instead of 3.0\n\n", i, C[i]);
 			exit(1);
 		}
 	}	
